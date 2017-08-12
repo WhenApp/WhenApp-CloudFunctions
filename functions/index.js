d79b75f2df03587acd14e48e44169bcb3883fa83
Austin Peterson <austin@akpwebdesign.com>
2017-08-11 21:50:51 -0400

    feat: cleanDatabase() and checkTask()
    `cleanDatabase()` runs when a specific pubsub topic is published to, and cleans the
    database of any events that don't meet all requirements of a task.
    
    `checkTask()` runs any time a new task is added, and removes it if it doesn't meet
    the requirements.

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,33 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp(functions.config().firebase);
+const rootRef = admin.database().ref();
+
+/**
+ * Cleans the database, removing any tasks that don't meet the requirements of a task.
+ */
+exports.cleanDatabase = functions.pubsub.topic('clean-database').onPublish((event) => {
+  rootRef.child('events/').on('child_added', (e) => {
+    e.ref.on('child_added', (t) => {
+      let task = t.val();
+      if(!task.name.length) {
+        t.ref.remove();
+      }
+    });
+  });
+});
+
+/**
+ * Checks each incoming task to make sure that it meets any requirements before letting it be saved.
+ */
+exports.checkTask = functions.database.ref('/events/{uid}/{taskID}').onWrite((event) => {
+  let task = event.data.val();
+  
+  if(!task.name.length) {
+    if(event.data.previous.exists()) {
+      return event.data.ref.set(event.data.previous.val());
+    }
+    return event.data.ref.remove();
+  }
+});
