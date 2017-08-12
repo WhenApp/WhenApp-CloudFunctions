c965b198a1d01db96798625c9f4deded4e18f032
Austin Peterson <austin@akpwebdesign.com>
2017-08-11 23:05:04 -0400

    fix: stop checkTask from running unnecessarily

--- functions/index.js
+++ functions/index.js
@@ -8,30 +8,38 @@ const rootRef = admin.database().ref();
  * Cleans the database, removing any tasks that don't meet the requirements of a task.
  */
 exports.cleanDatabase = functions.pubsub.topic('clean-database').onPublish((event) => {
-  return rootRef.child('events/').on('child_added', (e) => {
-    return e.ref.on('child_added', (t) => {
-      let task = t.val();
-      if(!task.name.length) {
-        return t.ref.remove();
-      }
+  return rootRef.child('events/').once('value').then((users) => {
+    users.forEach((user) => {
+      console.log('processing user ' + user.key);
+      user.forEach((t) => {
+        console.log('processing task ' + t.key);
+        let task = t.val();
+        if(!task.name.length) {
+          t.ref.remove();
+        }
+      });
     });
   });
+});
+
+exports.checkTaskOnCreate = functions.database.ref('/events/{uid}/{taskID}').onCreate((event) => {
+  checkTask(event, false);
+});
+
+exports.checkTaskOnUpdate = functions.database.ref('/events/{uid}/{taskID}').onUpdate((event) => {
+  checkTask(event, true);
 });
 
 /**
  * Checks each incoming task to make sure that it meets any requirements before letting it be saved.
  */
-exports.checkTask = functions.database.ref('/events/{uid}/{taskID}').onWrite((event) => {
-  if(!event.data.exists()) {
-    return;
-  }
-
+function checkTask(event, update) {
   let task = event.data.val();
   
   if(!task.name.length) {
-    if(event.data.previous.exists()) {
+    if(update && event.data.previous.exists()) {
       return event.data.ref.set(event.data.previous.val());
     }
     return event.data.ref.remove();
   }
-});
+}
